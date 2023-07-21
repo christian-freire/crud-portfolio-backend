d6a70d03d6c56b2f7270341e965cceb3e56e0e3e
Christian Evangelista Freire <74272121+christian-freire@users.noreply.github.com>
2023-07-21 17:28:06 -0300

    [feat] Update user implement

---
+++ controllers/updateUser.go
@@ -0,0 +1,11 @@
+package controllers
+
+import (
+	"projetos-pessoais/crud-porfolio-backend/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UpdateUserByID(c *gin.Context) {
+	var users models.User
+}
